2d64ccbb7116fee114c14c765948df46388965dc
maheshbusireddy <busireddymahesh@gmail.com>
2023-07-05 09:49:08 +0530

    add

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,22 +2,4 @@ def compile() {
   if (app_lang == "nodejs") {
      sh 'npm install'
   }   
-
-
-  if (app_lang == "maven") {
-     sh 'maven package'
-  } 
-}   
-
-
-  def unittest() {
-  if (app_lang == "nodejs") {
-     //sh 'npm test'
-     sh 'echo test cases'
-  }   
-
-
-  if (app_lang == "maven") {
-     sh 'maven test'
-  } 
 }
