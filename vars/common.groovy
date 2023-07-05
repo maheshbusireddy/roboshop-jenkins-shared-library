370f88ad72ceeec2cda8e51b732a73f3f35d77f2
maheshbusireddy <busireddymahesh@gmail.com>
2023-07-05 11:20:29 +0530

    add unittestcases

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,4 +2,17 @@ def compile() {
   if (app_lang == "nodejs") {
      sh 'npm install'
   }   
+   if (app_lang == "maven") {
+     sh 'mvn package'
+  }   
+} 
+
+def unittests() {
+
+   if (app_lang == "nodejs") {
+     sh 'npm install'
+  }
+   if (app_lang == "maven") {
+     sh 'mvn package'
+  } 
 }
