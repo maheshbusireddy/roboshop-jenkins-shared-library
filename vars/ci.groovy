4294c95406fe6fddcab77f148ece1ea0d79d1773
maheshbusireddy <busireddymahesh@gmail.com>
2023-07-08 19:30:44 +0530

    added some tests

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -4,7 +4,7 @@ def call() {
 
       stage('Checkout') {
         cleanWs()
-        git branch: 'main', url: 'https://github.com/maheshbusireddy/${component}.git'
+        git branch: 'main', url: 'https://github.com/maheshbusireddy/cart.git'
        }
 
        stage('Compile/Build'){
