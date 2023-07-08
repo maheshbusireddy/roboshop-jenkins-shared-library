6233678b2ea4f829339f4977cd1e310c07d48ac1
maheshbusireddy <busireddymahesh@gmail.com>
2023-07-08 18:06:45 +0530

    added some tests

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,30 +1,18 @@
 def call() {
  try {
-
- pipeline {
-    agent {
-        label 'workstation'
-    }
-
-    stages {
-    
-       stage('Compile/Build') {
-         steps {
-            script {
-                common.compile()
-            }
-         }
+    node('workstatio') {
+       stage('Checkout') {
+        cleanWs()
+        git branch: 'main', url: 'https://github.com/maheshbusireddy/cart.git'
        }
 
-       stage('Unit tests') {
-         steps {
-            script {
-                common.unittests()
-            }
-         }
+       stage('Compile/Build'){
+        common.compile()
        }
-
-       stage('Quality Control') {
+       stage('Unit Tests'){
+        common.unittests()
+       }
+        stage('Quality Control') {
          environment {
             SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
             //SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
@@ -35,9 +23,7 @@ def call() {
               wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
           sh "sonar-scanner -Dsonar.host.url=http://172.31.95.25:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
             }
-         }
-       }
-
+    }
        stage('Upload Code To The Centralized Repo') {
          steps {
             echo 'upload'
@@ -45,6 +31,11 @@ def call() {
        }
     }
 }
- }
- }
+} catch(Exception e) {
+    common.email("Failed")
+  }
 }
+
+ 
+ 
+
