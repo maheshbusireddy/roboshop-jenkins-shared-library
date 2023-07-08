53e68531e6200763b731473fa319caac6daf6d53
maheshbusireddy <busireddymahesh@gmail.com>
2023-07-08 17:06:19 +0530

    added some tests

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -31,7 +31,7 @@ def call() {
           }
           steps {
            
-            sh "sonar-scanner -Dsonar.host.url=http://172.31.95.25:9000 -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASS} -Dsonar.projectKey=cart"
+            sh "sonar-scanner -Dsonar.host.url=http://172.31.95.25:9000 -Dsonar.login=${SONAR_USER} -Dsonar.password=${SONAR_PASS} -Dsonar.projectKey=cart"
 
          }
        }
