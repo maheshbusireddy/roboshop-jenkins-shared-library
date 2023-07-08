137978a6759651b2981d7b40cab902079d4f25f4
maheshbusireddy <busireddymahesh@gmail.com>
2023-07-08 18:29:09 +0530

    added some tests

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,8 +1,8 @@
 def call() {
  try {
-    node('workstation') {
+   node('workstation') {
 
-       stage('Checkout') {
+      stage('Checkout') {
         cleanWs()
         git branch: 'main', url: 'https://github.com/maheshbusireddy/cart.git'
        }
@@ -15,7 +15,6 @@ def call() {
         common.unittests()
        }
         stage('Quality Control') {
-         environment {
             SONAR_USER = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
             SONAR_PASS = '$(aws ssm get-parameters --region us-east-1 --names sonarqube.pass  --with-decryption --query Parameters[0].Value | sed \'s/"//g\')'
             wrap([$class: 'MaskPasswordsBuildWrapper', varPasswordPairs: [[password: "${SONAR_PASS}", var: 'SECRET']]]) {
@@ -24,16 +23,14 @@ def call() {
          }
     
        stage('Upload Code To The Centralized Repo') {
-         steps {
-            echo 'upload'
-         }
+        echo 'upload'
        }
-    } catch(Exception e) {
-    common.email("Failed")
-  }
+         }
+       } catch(Exception e) {
+         common.email("Failed")
+       }
 }
- }
-}
+ 
 
  
  
